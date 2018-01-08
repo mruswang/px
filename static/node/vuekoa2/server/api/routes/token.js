1d1d4c941f905baf67940340a81334eec0864384
mruswang <875484737@qq.com>
2018-01-08 21:19:46 +0800

    '.'

--- static/node/vuekoa2/server/api/routes/token.js
+++
@@ -1,6 +0,0 @@
-import * as $ from '../../controllers/token_controller.js'
-
-export default async(router) => {
-  $.initUser()
-  router.post('/token', $.login)
-}
--- static/node/vuekoa2/server/start.js
+++
@@ -1,8 +0,0 @@
-require("babel-core/register")({
-     presets: ['es2015', 'stage-0']
-});
-require("babel-polyfill");
-
-module.exports = require('./index.js');
-
-
